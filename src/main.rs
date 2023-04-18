4ff025f1b38f74a1d558b421f4c31e12c0a27480
marlion01 <fantand.greeeen.mass@gmail.com>
2023-04-18 20:38:33 +0900

    localhost:3000番叩くとhello worldと出るようにしました

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ use axum::{routing::get,Router};
 use std::net::SocketAddr;
 #[tokio::main]
 async fn main(){
-    init_logger();
     let app=Router::new().route("/", get(root));//アプリケーションのルーティング設定　メソッド設定はgetやpostに変更できる
     //またget(get_hundler).post(post_hundler）のようにメソッドチェーンで呼び起こす事ができる
     let adder=SocketAddr::from(([127,0,0,1],3000));//IPAdder from関数で[]でIPを指定し第二因数でポート番号を指定できる
-    log::debug!("some debu log");
     axum::Server::bind(&adder)//adder(IPアドレス)をサーバーにバインドしている
      .serve(app.into_make_service())//サーバーが立ち上がる
      .await
@@ -14,8 +12,4 @@ async fn main(){
 }
 async fn root()->&'static str{//Hello world を返すだけなのでstrings指定である。Jsonやbodyを返すこともできる
     "Hello World"
-}
-fn init_logger(){
-    //loggingの初期化
-    env_logger::init();
 }
